fa08ab18fe201a422e192a0a511df61681658198
stephguac <stephaniefkwok@gmail.com>
2016-04-27 16:25:05 -0700

    commit

--- application/models/Cart.php
+++ application/models/Cart.php
@@ -1,7 +1,13 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cart extends CI_Model 
-{
+class Cart extends CI_Model {
+
+	public function productAddToCart($productID) {
+		$sql = "INSERT INTO cart_product (cart_id, product_id) VALUES (?, ?)";
+		$params = [
+			
+		];	
+	}
 
 }
