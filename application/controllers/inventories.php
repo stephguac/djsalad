c5905f54bd77342dbb6b89edbaef3e46d5fd76a3
ryugie <ryugie@gmail.com>
2016-04-27 09:55:34 -0700

    renamed some files for consistency

--- application/controllers/inventories.php
+++ application/controllers/inventories.php
@@ -5,7 +5,7 @@ class Inventories extends CI_Controller {
 	
 	public function __construct() {
         parent::__construct();
-        $this->load->model('MODELNAME');
+        $this->load->model('Inventory');
     }
 
 	public function index() {
--- application/models/Inventory.php
+++ application/models/Inventory.php
@@ -1,7 +1,7 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Product extends CI_Model 
+class Inventory extends CI_Model 
 {
 	public function index(){
 		$this->load->view('productDetailsView.php');
